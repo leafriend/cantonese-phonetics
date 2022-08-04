e6bf365a18ea66841d04a47e4cf72c51dc3874ec
leafriend <leafriend@gmail.com>
2022-08-04 10:21:30 +0900

    Add states for from/to

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import './App.scss';
 function App() {
 
   const [input, setInput] = React.useState('');
+  const [from, setFrom] = React.useState('Jyutping');
+  const [to, setTo] = React.useState('SidneyLau');
   const [result, setResult] = React.useState('');
 
   function inputChanged(value: string) {
@@ -12,11 +14,13 @@ function App() {
 
   function convert(): void {
     const result = input;
+    console.log(`Convert ${from} to ${to}`);
     setResult(result);
   }
 
   return (
     <div className='App'>
+
       <div className='input'>
         <textarea
           placeholder='[&#10;["信", "seon3"]&#10;]'
@@ -24,23 +28,26 @@ function App() {
           value={input}
         ></textarea>
       </div>
-      <div className='button'>
-        <select>
+
+      <div className='actions'>
+        <select value={from} onChange={e => setFrom(e.target.value)}>
           <option value='Jyutping'>粵拼</option>
         </select>
         <button onClick={_ => convert()}>Convert</button>
-        <select>
+        <select value={to} onChange={e => setTo(e.target.value)}>
           <option value='Jyutping'>粵拼</option>
-          {/* <option value='SidneyLau'>劉錫祥</option> */}
+          <option value='SidneyLau'>劉錫祥</option>
         </select>
       </div>
+
       <div className='result'>
-      <textarea
-        placeholder='[&#10;["信", "seun"]&#10;]'
-        value={result}
-        readOnly
-      ></textarea>
+        <textarea
+          placeholder='[&#10;["信", "seun"]&#10;]'
+          value={result}
+          readOnly
+        ></textarea>
       </div>
+
     </div>
   );
 }
