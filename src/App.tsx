47df7c0ef35a2057c8891955520aae7d8380bf34
leafriend <leafriend@gmail.com>
2022-08-04 09:42:02 +0900

    Add basic form elements

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,21 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
+      <div className="input">
+        <textarea placeholder='[&#10;["信", "seon3"]&#10;]'></textarea>
+      </div>
+      <div className="button">
+        <select>
+          <option value="Jyutping">粵拼</option>
+        </select>
+        <button>Convert</button>
+        <select>
+          <option value="SidneyLau">劉錫祥</option>
+        </select>
+      </div>
+      <div className="output">
+      <textarea placeholder='[&#10;["信", "seun"]&#10;]'></textarea>
+      </div>
     </div>
   );
 }
